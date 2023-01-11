3fdd75582b9deba85e0debe8a51863e5ea2287a5
ginalzsolt <ginalzsolt@turr.hu>
2023-01-11 09:30:02 +0100

    basic board

--- Dama/Form1.cs
+++ Dama/Form1.cs
@@ -15,6 +15,43 @@ namespace Dama
         public Form1()
         {
             InitializeComponent();
+
+            drawboard();
+
+        }
+
+
+        private void drawboard()
+        {
+            
+            this.MinimumSize= new Size(8 * 80 + 15, 8 * 80 + 35);
+            this.MaximumSize= new Size(8 * 80 + 15, 8 * 80 + 35);
+            
+            bool indent = false;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    PictureBox pbx = new PictureBox();
+                    pbx.Name = $"_{i}_{j}";
+                    pbx.Width = 80;
+                    pbx.Height = 80;
+                    if (indent)
+                    {
+                        pbx.Left = 80 + j * 160;
+                        pbx.Top=80*i;
+                    }
+                    else
+                    {
+                        pbx.Left =  j * 160;
+                        pbx.Top = 80 * i;
+                    }
+                    pbx.Image = Properties.Resources.FeherDama;
+                    pbx.SizeMode = PictureBoxSizeMode.Zoom;
+                    Controls.Add(pbx);
+                }
+                indent = !indent;
+            }
         }
     }
 }
