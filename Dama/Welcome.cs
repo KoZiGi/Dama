7bce0f82692c2402f90e8466d47d4b6af9a68f97
zickbalazs <zickbalazs@turr.hu>
2023-01-11 10:01:30 +0100

    Welcome form kész

---
+++ Dama/Welcome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dama
+{
+    public partial class Welcome : Form
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            Process.Start("https://hu.wikipedia.org/wiki/D%C3%A1maj%C3%A1t%C3%A9k");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
