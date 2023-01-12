ee105190db57261159078e314f86596a56f02572
zickbalazs <zickbalazs@turr.hu>
2023-01-12 13:09:18 +0100

    Kijelölt képek hozzáadva (mozgás)

---
+++ Dama/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dama
+{
+    class Data
+    {
+        public static DamaPiece[,] Field = new DamaPiece[8, 8];
+        public static DamaPiece selected;
+    }
+}
---
+++ Dama/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dama
+{
+    class Game
+    {
+
+
+
+        public void GameLogic(bool moves)
+        {
+            if (moves) SelectPiece();
+            else MovePiece();
+        }
+        public static void SelectPiece()
+        {
+
+        }
+        public static  void MovePiece()
+        {
+
+        }
+
+    }
+}
